7d4b4e1cf438058c31b494abafae5d5a466c18bb
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2023-07-02 19:36:20 +0900

    Upgrade second version to only use 1 local variable.

--- src/solutions/s0026_remove_duplicates_from_sorted_array/version_02.rs
+++ src/solutions/s0026_remove_duplicates_from_sorted_array/version_02.rs
@@ -2,24 +2,15 @@ pub struct Solution;
 
 impl Solution {
     pub fn remove_duplicates_02(nums: &mut Vec<i32>) -> i32 {
-        let len = nums.len();
-
-        if len <= 1 {
-            return len as i32;
-        }
-
-        let mut unique_count = 1;
-
         let mut current = 0;
-        for next in 1..nums.len() {
+        for next in 0..nums.len() {
             if nums[next] > nums[current] {
-                unique_count += 1;
-                nums[current + 1] = nums[next];
                 current += 1;
+                nums[current] = nums[next];
             }
         }
-        
-        unique_count
+        current += 1;
+        current as i32
     }
 }
 
@@ -29,8 +20,8 @@ mod tests {
 
     #[test]
     fn test() {
-        let mut nums = vec![1, 1, 2, 3, 3, 3, 4, 4];
-        let expected_nums = vec![1, 2, 3, 4];
+        let mut nums = vec![1,2,2,3,3,4,4,5,5];
+        let expected_nums = vec![1,2,3,4,5];
         let unique_count = Solution::remove_duplicates_02(&mut nums);
         assert_eq!(unique_count, expected_nums.len() as i32);
         assert_eq!(nums.into_iter().take(unique_count as usize).collect::<Vec<i32>>(), expected_nums);
