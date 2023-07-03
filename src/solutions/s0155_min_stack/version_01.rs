e8c63e1a7cb00dcb1768e994f464c1466f4c1483
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2023-07-03 13:53:52 +0900

    Update Min Stack solution for only use two stack.

--- src/solutions/s0155_min_stack/version_01.rs
+++ src/solutions/s0155_min_stack/version_01.rs
@@ -1,58 +1,47 @@
 struct MinStack {
-    top: i32,
-    min: i32,
     stack: Vec<i32>,
     min_stack: Vec<i32>,
 }
 
 /**
-  * `&self` means the method takes an immutable reference
-  * If you need a mutable reference, change it to `&mut self` instead.
+ * `&self` means the method takes an immutable reference
+ * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl MinStack {
     fn new() -> Self {
         MinStack {
-            top: -1,
-            min: i32::MAX,
             stack: Vec::new(),
             min_stack: Vec::new(),
         }
     }
 
     fn push(&mut self, val: i32) {
-        self.top += 1;
         self.stack.push(val);
-        if val <= self.min {
-            self.min = val;
-            self.min_stack.push(val);
+        match self.min_stack.last() {
+            // Handle duplicates min
+            Some(min) if val <= *min => {
+                self.min_stack.push(val);
+            }
+            None => self.min_stack.push(val),
+            _ => (),
         }
     }
 
     fn pop(&mut self) {
-        if self.top == -1 {
-            return;
-        }
-        self.top -= 1;
-        let last = self.stack.pop().unwrap();
-        if last == self.min {
-            self.min_stack.pop();
-            if let Some(min) = self.min_stack.last() {
-                self.min = *min;
-            } else {
-                self.min = i32::MAX;
-            }
+        match (self.stack.pop(), self.min_stack.last()) {
+            (Some(val), Some(min)) if val == *min => {
+                self.min_stack.pop(); 
+            },
+            _ => {}
         }
     }
 
     fn top(&self) -> i32 {
-        if self.top == -1 {
-            return 0;
-        }
-        self.stack[self.top as usize].clone()
+        self.stack.last().unwrap().clone()
     }
 
     fn get_min(&self) -> i32 {
-        self.min
+        self.min_stack.last().unwrap().clone()
     }
 }
 
@@ -64,15 +53,13 @@ impl MinStack {
  * let ret_3: i32 = obj.top();
  * let ret_4: i32 = obj.get_min();
  */
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn test() {
         let mut obj = MinStack::new();
-        assert_eq!(obj.top(), 0);
         obj.push(0);
         obj.push(1);
         obj.push(0);
