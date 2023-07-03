0ab47382672edfc489b04d0623adc297ed2ee402
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2023-07-03 20:28:08 +0900

    Refactoring.

--- src/solutions/s0206_reserve_linked_list/version_01.rs
+++ src/solutions/s0206_reserve_linked_list/version_01.rs
@@ -1,6 +1,3 @@
-use std::arch::x86_64::_mm_sha1nexte_epu32;
-use std::mem::swap;
-
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -56,15 +53,13 @@ mod tests {
         let mut second = Box::new(ListNode::new(2));
         let mut third = Box::new(ListNode::new(3));
         let mut fourth = Box::new(ListNode::new(4));
-        let mut fifth = Box::new(ListNode::new(5));
+        let fifth = Box::new(ListNode::new(5));
         fourth.next = Some(fifth);
         third.next = Some(fourth);
         second.next = Some(third);
         first.next = Some(second);
 
-        let mut head = Some(first);
-        let mut reserved_head = Solution::reverse_list(head);
-
+        let mut reserved_head = Solution::reverse_list(Some(first));
         let mut result = vec![];
         while let Some(node) = reserved_head {
             result.push(node.val);
